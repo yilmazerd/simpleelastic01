ebf337f353db6af99045cd83deb4084c3e4ff047
yilmazerd <yilmazerdem@gmail.com>
2020-11-22 01:27:23 -0500

    Add header controls

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,11 +17,19 @@ import java.nio.file.Paths;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class Application extends AbstractHandler
 {
     private static final int PAGE_SIZE = 3000;
     private static final String INDEX_HTML = loadIndex();
+    private static final String DEFAULT_RESPONSE = "200";
+    private static final String DEFAULT_DELAY = "0";
+    private static final String RESPONSE_CODE_HEADER = "responseCode";
+    private static final String DELAY_HEADER = "responseDelay";
+    private static final String URL_HEADER = "urlHeader";
+    RestTemplate restTemplate = new RestTemplate();
 
     private static String loadIndex() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(Application.class.getResourceAsStream("/index.html")))) {
@@ -55,7 +64,16 @@ public class Application extends AbstractHandler
 
     private void handleHttpRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // Handle HTTP requests here.
-        response.getWriter().println(INDEX_HTML);
+        // Get response code
+        String resourceURL = request.getHeader(URL_HEADER);
+        String responseFromUrl = null;
+
+        if (resourceURL != null) {
+            ResponseEntity<String> responseCall = restTemplate.getForEntity(resourceURL, String.class);
+            responseFromUrl = responseCall.getBody();
+        }
+
+        response.getWriter().println(ObjectUtils.firstNonNull(responseFromUrl,"Sample Response"));
     }
 
     private void handleCronTask(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -66,8 +84,20 @@ public class Application extends AbstractHandler
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+        //response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
+
+        // Get response code
+        String responseCode = ObjectUtils.firstNonNull(baseRequest.getHeader(RESPONSE_CODE_HEADER),DEFAULT_RESPONSE);
+        response.setStatus(Integer.valueOf(responseCode));
+
+        // Apply delay
+        String responseDelay = ObjectUtils.firstNonNull(baseRequest.getHeader(DELAY_HEADER),DEFAULT_DELAY);
+        try {
+            Thread.sleep(Integer.valueOf(responseDelay));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         String pathInfo = request.getPathInfo();
         if (pathInfo.equalsIgnoreCase("/crontask")) {
